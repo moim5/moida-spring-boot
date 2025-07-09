d4b6397631e2d320829bd0e5f7de62350832f4c7
Castleman-Jang <andysign@naver.com>
2025-07-09 19:32:45 +0900

    장성남 맵핑

--- src/main/java/com/kh/moida/controller/HomeController.java
+++ src/main/java/com/kh/moida/controller/HomeController.java
@@ -9,4 +9,24 @@ public class HomeController {
     public String home() {
         return "pages/home";
     }
+    
+    @GetMapping("/createMoim")
+    public String CreateMoim() {
+    	return "pages/CreateMoim";
+    }
+    
+    @GetMapping("/")
+    public String JoinMoim() {
+    	return "pages/JoinMoim";
+    }
+    
+    @GetMapping("/")
+    public String modifyMoim() {
+    	return "pages/modifyMoim";
+    }
+    
+    @GetMapping("/")
+    public String personalInfo() {
+    	return "pages/personalInfo";
+    }
 }
