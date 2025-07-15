c5c09f57819067f9b4779c1437fb7d1d24b25ca8
이기정 <163862000+LeeKijung2000@users.noreply.github.com>
2025-07-15 20:43:42 +0900

    noticeController 및 page 작성 but 권한오류

--- src/main/java/com/kh/moida/notice/NoticeController.java
+++ src/main/java/com/kh/moida/notice/NoticeController.java
@@ -1,8 +1,19 @@
 package com.kh.moida.notice;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("notice")
 public class NoticeController {
+
+    @GetMapping("list")
+    public String list() {
+        System.out.println("true = " + true);
+        return "pages/notice/list";
+    }
 
 }
