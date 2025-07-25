b83b0facff95d16f85d5b8e2b2cc55ca51f3dff1
jiwon Lee <leejw1757@naver.com>
2025-07-25 17:35:53 +0900

    BACK : 후기수정,등록 수정 중

--- src/main/java/com/kh/moida/mapper/ReviewMapper.java
+++ src/main/java/com/kh/moida/mapper/ReviewMapper.java
@@ -3,6 +3,7 @@ package com.kh.moida.mapper;
 import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.moida.model.Review;
 
@@ -14,5 +15,11 @@ public interface ReviewMapper {
 	
 	//리뷰 이미 작성했을 경우 write view이동 막기
 	public int countReview(int reviewId);
+
+	//후기등록
+	public int enrollReview(Review r, MultipartFile image);
+
+	//후기 수정
+	public Review selectReview(int reviewId);
 
 }
--- src/main/java/com/kh/moida/service/ReviewService.java
+++ src/main/java/com/kh/moida/service/ReviewService.java
@@ -3,6 +3,7 @@ package com.kh.moida.service;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.moida.mapper.ReviewMapper;
 import com.kh.moida.model.Review;
@@ -23,6 +24,16 @@ public class ReviewService {
 	//리뷰 이미 작성했을 경우 write view이동 막기
 	public int countReview(int reviewId) {
 		return mapper.countReview(reviewId);
+	}
+
+	//후기 등록(write, edit 등록 합침)
+	public int enrollReview(Review r, MultipartFile image) {
+		return mapper.enrollReview(r, image);
+		
+	}
+	//후기 수정페이지 이동
+	public Review selectReview(int reviewId) {
+		return mapper.selectReview(reviewId);
 	}
 
 }
