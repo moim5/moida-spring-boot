d3a4e610702c9344e83ec3ee6cad5eb0dac41ae1
Tae.Garam (MacBook) <drasla217@gmail.com>
2025-08-03 08:55:45 +0900

    BACK : 버그 수정

--- src/main/java/com/kh/moida/dto/ReviewWithUser.java
+++ src/main/java/com/kh/moida/dto/ReviewWithUser.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 @Setter
 @ToString
 public class ReviewWithUser {
-	 private int reviewId;
+	 private Long reviewId;
 	 @DateTimeFormat(pattern = "yyyy-MM-dd")
 	 private Date reviewDate;
 	 private String reviewTitle;
