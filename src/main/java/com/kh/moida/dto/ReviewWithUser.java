dcbb4fa476f0af7dd0f8c3b0fc6a21c28678bc32
jiwon Lee <leejw1757@naver.com>
2025-08-05 15:58:04 +0900

    BACK : review_edit , read완료(DTO :file convert,file Origin추가함)

--- src/main/java/com/kh/moida/dto/ReviewWithUser.java
+++ src/main/java/com/kh/moida/dto/ReviewWithUser.java
@@ -6,10 +6,12 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
